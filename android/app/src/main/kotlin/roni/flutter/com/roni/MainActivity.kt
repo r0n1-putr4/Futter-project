83d02553508315df17ea48619bd772153564a45b
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-02-26 21:11:39 +0700

    Update Code

---
+++ android/app/src/main/kotlin/roni/flutter/com/roni/MainActivity.kt
@@ -0,0 +1,5 @@
+package roni.flutter.com.roni
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/berita/list_berita.dart
+++ lib/berita/list_berita.dart
@@ -1,8 +1,11 @@
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:roni/model/berita_model.dart';
 import 'package:roni/utils/base_url.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:logger/logger.dart';
 
 class ListBerita extends StatefulWidget {
   const ListBerita({super.key});
@@ -13,13 +16,15 @@ class ListBerita extends StatefulWidget {
 
 class _ListBeritaState extends State<ListBerita> {
   late Future<List<Datum>?> dataJson;
+  var logger = Logger();
 
   Future<List<Datum>?> getData() async {
     try {
       http.Response hasil = await http.get(
         Uri.parse("${ApiConfig.baseUrl}/get_berita.php"),
       );
-
+      logger.d("Status ${beritaModelFromJson(hasil.body).success}");
+      print("Status ${beritaModelFromJson(hasil.body).success}");
       return beritaModelFromJson(hasil.body).data;
     } catch (e) {
       print("Kesalahan ${e}");
@@ -55,16 +60,64 @@ class _ListBeritaState extends State<ListBerita> {
             return ListView.builder(
               itemCount: berita.length,
               itemBuilder: (context, index) {
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  child: Row(
-                    children: [
-                      Image(
-                        image: NetworkImage(berita[index].gambar),
-                        width: 200,
-                      ),
-                      Expanded(child: Text(berita[index].isi, softWrap: false)),
-                    ],
+                return Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    elevation: 3,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // Aligns text to the top
+                      children: [
+                        Image.network(
+                          berita[index].gambar,
+                          width: 80, // Adjust width
+                          height: 80, // Adjust height
+                          fit: BoxFit.cover,
+                        ),
+                        SizedBox(width: 10), // Space between image and text
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                berita[index].judul,
+                                textAlign: TextAlign.left,
+                                style: TextStyle(fontSize: 24),
+                                softWrap: true, // Ensures text wraps
+                                overflow:
+                                    TextOverflow
+                                        .visible, // Ensures text is shown fully
+                              ),
+                              Text(
+                                berita[index].tglIndonesiaBerita,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Row(
+                                children: [
+                                  RatingBarIndicator(
+                                    rating: berita[index].rating,
+                                    itemBuilder:
+                                        (context, index) => Icon(
+                                          Icons.star,
+                                          color: Colors.amber,
+                                        ),
+                                    itemCount: 5,
+                                    itemSize: 15,
+                                    direction: Axis.horizontal,
+                                  ),
+                                ],
+                              ),
+                              SizedBox(height: 5),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               },
