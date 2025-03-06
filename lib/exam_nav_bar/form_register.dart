2308c39876a79940e6628c29d17945452a679473
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-03-06 22:50:38 +0700

    Update Code

--- lib/exam_nav_bar/form_register.dart
+++ lib/exam_nav_bar/form_register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class FormRegister extends StatefulWidget {
   const FormRegister({super.key});
@@ -8,8 +9,249 @@ class FormRegister extends StatefulWidget {
 }
 
 class _FormRegisterState extends State<FormRegister> {
+  TextEditingController fullname = TextEditingController();
+  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController tglLahir = TextEditingController();
+  TextEditingController password = TextEditingController();
+  String? valAgama, valJk;
+  final _formKey = GlobalKey<FormState>();
+
+  Future selectDate() async {
+    DateTime? pickDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1950),
+      lastDate: DateTime(2100),
+    );
+    if (pickDate != null) {
+      tglLahir.text = DateFormat("dd-MM-yyyy").format(pickDate);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(child: Text("Register", style: TextStyle(fontSize: 18))),
+                SizedBox(height: 10),
+                CostumeTextFormField(
+                  labelInput: "Full Name",
+                  controller: fullname,
+                  hintText: "Roni Putra",
+                  textInputType: TextInputType.text,
+                ),
+                SizedBox(height: 10),
+                CostumeTextFormField(
+                  labelInput: "User Name",
+                  controller: username,
+                  hintText: "r0n1",
+                  textInputType: TextInputType.text,
+                ),
+                SizedBox(height: 10),
+                CostumeTextFormField(
+                  labelInput: "Email",
+                  controller: email,
+                  hintText: "rn.putra@gmail.com",
+                  textInputType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 10),
+                CostumeTextFormField(
+                  labelInput: "Tanggal Lahir",
+                  controller: tglLahir,
+                  hintText: "dd-MM-yyyy",
+                  textInputType: TextInputType.datetime,
+                  onTap: () {
+                    selectDate();
+                  },
+                ),
+                SizedBox(height: 10),
+                CostumeTextFormField(
+                  labelInput: "Password",
+                  controller: password,
+                  hintText: "*****",
+                  textInputType: TextInputType.datetime,
+                  obscureText: true,
+                ),
+                SizedBox(height: 10),
+                Text("Pilih Agama", style: TextStyle(fontSize: 18)),
+                Container(
+                  alignment: Alignment.center,
+                  height: 65,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black54),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: DropdownButton(
+                    value: valAgama,
+                    underline: Container(),
+                    isExpanded: true,
+                    items:
+                        [
+                          "Islam",
+                          "Kristen Protestan",
+                          "Kristen Katolik",
+                          "Hindu",
+                          "Budha",
+                          "Konghucu",
+                        ].map((e) {
+                          return DropdownMenuItem(
+                            value: e,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(e),
+                            ),
+                          );
+                        }).toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        valAgama = val;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text("Pilih Jenis Kelamin", style: TextStyle(fontSize: 18)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Flexible(
+                      child: RadioListTile(
+                        value: "Laki-laki",
+                        groupValue: valJk,
+                        onChanged: (val) {
+                          setState(() {
+                            valJk = val;
+                          });
+                        },
+                        activeColor: Colors.blue,
+                        title: const Text("Laki-laki"),
+                      ),
+                    ),
+                    Flexible(
+                      child: RadioListTile(
+                        value: "Perempuan",
+                        groupValue: valJk,
+                        onChanged: (val) {
+                          setState(() {
+                            valJk = val;
+                          });
+                        },
+                        activeColor: Colors.blue,
+                        title: const Text("Perempuan"),
+                      ),
+                    ),
+                  ],
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    // Full width, height: 50
+                    minimumSize: Size(double.infinity, 50),
+                    backgroundColor: Colors.red,
+                    // Change button color
+                    foregroundColor: Colors.white, // Change text color
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (_formKey.currentState!.validate()) {
+                        if (valJk != null && valAgama != null) {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text("Data Register"),
+                                content: Column(
+                                  children: [
+                                    Text("Fullname : ${fullname.text}"),
+                                    Text("Username : ${username.text}"),
+                                    Text("Email : ${email.text}"),
+                                    Text("Password : ${password.text}"),
+                                    Text("Agamma : $valAgama"),
+                                    Text("Jenis Kelamin : $valJk"),
+                                    Text("Tanggal Lahir : ${tglLahir.text}"),
+                                  ],
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("Dismiss"),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Pilih agama dang jenis kelamin"),
+                              backgroundColor: Colors.green,
+                            ),
+                          );
+                        }
+                      }
+                    });
+                  },
+                  child: Text("SAVE", style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CostumeTextFormField extends StatelessWidget {
+  final String labelInput;
+  final TextEditingController controller;
+  final String hintText;
+  final TextInputType textInputType;
+  final VoidCallback? onTap;
+  final bool obscureText;
+
+  const CostumeTextFormField({
+    super.key,
+    required this.labelInput,
+    required this.controller,
+    required this.hintText,
+    required this.textInputType,
+    this.onTap,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(labelInput, style: TextStyle(fontSize: 18)),
+        SizedBox(height: 5),
+        TextFormField(
+          controller: controller,
+          validator: (val) {
+            return val!.isEmpty ? "Tidak boleh kosong" : null;
+          },
+          decoration: InputDecoration(
+            hintText: hintText,
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          ),
+          keyboardType: textInputType,
+          onTap: () {
+            onTap?.call();
+          },
+          obscureText: obscureText,
+        ),
+      ],
+    );
   }
 }
