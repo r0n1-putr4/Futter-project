2bc31d251334708ef911d07de961ef35591bfa49
r0n1-putr4 <82272376+r0n1-putr4@users.noreply.github.com>
2025-04-11 09:49:10 +0700

    Update Code

--- lib/map/map_marker_page.dart
+++ lib/map/map_marker_page.dart
@@ -9,33 +9,34 @@ class MapMarkerPage extends StatefulWidget {
 }
 
 class _MapMarkerPageState extends State<MapMarkerPage> {
+  Set<Marker> _markers = {};
 
-  final List<Marker> _marker = [];
-  final List<Marker> _markerList = [
-    Marker(
-      markerId: MarkerId("Tempat 1, Sumbar"),
-      position: LatLng(-0.9485196050120748, 100.3609429170195),
-      infoWindow: InfoWindow(
-        title: 'Kota Padang, Sumbar',
-        snippet: 'Padang',
+  Future<void> _loadCustomMarkers() async {
+    final markers = <Marker>{
+      Marker(
+        markerId: MarkerId("Tempat 1, Sumbar"),
+        position: LatLng(-0.9485196050120748, 100.3609429170195),
+        infoWindow: InfoWindow(title: 'Kota Padang, Sumbar', snippet: 'Padang'),
       ),
-    ),
-    Marker(
-      markerId: MarkerId("Tempat 1, Sumbar"),
-      position: LatLng(-0.9452317941385855, 100.36321441807704),
-      infoWindow: InfoWindow(
-        title: 'Kota Padang, Sumbar',
-        snippet: 'Padang',
+      Marker(
+        markerId: MarkerId("Tempat 1, Sumbar"),
+        position: LatLng(-0.9452317941385855, 100.36321441807704),
+        infoWindow: InfoWindow(title: 'Kota Padang, Sumbar', snippet: 'Padang'),
       ),
-    ),
-  ];
+    };
+
+    setState(() {
+      _markers = markers;
+    });
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _marker.addAll(_markerList);
+    _loadCustomMarkers();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +45,7 @@ class _MapMarkerPageState extends State<MapMarkerPage> {
           target: LatLng(-0.9472813143938821, 100.36326840320734),
           zoom: 16,
         ),
-        markers: Set<Marker>.of(_markerList),
+        markers: _markers,
       ),
     );
   }
